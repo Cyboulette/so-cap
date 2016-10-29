0152c50d85fb17b707d8ce4191b3f9d619142088
Cyboulette <cyboulette58@gmail.com>
2016-10-29 17:53:06 +0200

    Update DB

--- dump_sql.sql
+++ dump_sql.sql
@@ -1,22 +1,23 @@
 -- phpMyAdmin SQL Dump
--- version 3.3.2deb1ubuntu1
+-- version 4.5.4.1deb2ubuntu2
 -- http://www.phpmyadmin.net
 --
--- Serveur: localhost
--- Généré le : Dim 23 Octobre 2016 à 17:28
--- Version du serveur: 5.1.73
--- Version de PHP: 5.5.0
+-- Client :  localhost
+-- Généré le :  Sam 29 Octobre 2016 à 17:52
+-- Version du serveur :  5.7.16-0ubuntu0.16.04.1
+-- Version de PHP :  7.0.8-0ubuntu0.16.04.3
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données: `railot`
+-- Base de données :  `railot`
 --
 
 -- --------------------------------------------------------
@@ -25,18 +26,11 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- Structure de la table `categories`
 --
 
-CREATE TABLE IF NOT EXISTS `categories` (
-  `idCategorie` int(11) NOT NULL AUTO_INCREMENT,
-  `label` text NOT NULL,
-  PRIMARY KEY (`idCategorie`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
-
---
--- Contenu de la table `categories`
---
-
-INSERT INTO `categories` (`idCategorie`, `label`) VALUES
-(1, 'Défaut');
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE `categories` (
+  `idCategorie` int(11) NOT NULL,
+  `label` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -44,19 +38,13 @@ INSERT INTO `categories` (`idCategorie`, `label`) VALUES
 -- Structure de la table `commandes`
 --
 
-CREATE TABLE IF NOT EXISTS `commandes` (
-  `idCommande` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `commandes`;
+CREATE TABLE `commandes` (
+  `idCommande` int(11) NOT NULL,
   `idUtilisateur` int(11) NOT NULL,
   `dateCommande` datetime NOT NULL,
-  `prixTotal` int(11) NOT NULL,
-  PRIMARY KEY (`idCommande`),
-  KEY `idUtilisateur` (`idUtilisateur`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
---
--- Contenu de la table `commandes`
---
-
+  `prixTotal` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -64,20 +52,14 @@ CREATE TABLE IF NOT EXISTS `commandes` (
 -- Structure de la table `livraisons`
 --
 
-CREATE TABLE IF NOT EXISTS `livraisons` (
-  `idLivraison` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `livraisons`;
+CREATE TABLE `livraisons` (
+  `idLivraison` int(11) NOT NULL,
   `idCommande` int(11) NOT NULL,
   `date` datetime NOT NULL,
   `etatCommande` text NOT NULL,
-  `modeLivraison` int(11) NOT NULL,
-  PRIMARY KEY (`idLivraison`),
-  KEY `modeLivraison` (`modeLivraison`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
---
--- Contenu de la table `livraisons`
---
-
+  `modeLivraison` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -85,16 +67,11 @@ CREATE TABLE IF NOT EXISTS `livraisons` (
 -- Structure de la table `modesLivraisons`
 --
 
-CREATE TABLE IF NOT EXISTS `modesLivraisons` (
-  `idModeLivraison` int(11) NOT NULL AUTO_INCREMENT,
-  `label` text NOT NULL,
-  PRIMARY KEY (`idModeLivraison`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
---
--- Contenu de la table `modesLivraisons`
---
-
+DROP TABLE IF EXISTS `modesLivraisons`;
+CREATE TABLE `modesLivraisons` (
+  `idModeLivraison` int(11) NOT NULL,
+  `label` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -102,22 +79,15 @@ CREATE TABLE IF NOT EXISTS `modesLivraisons` (
 -- Structure de la table `produits`
 --
 
-CREATE TABLE IF NOT EXISTS `produits` (
-  `idProduit` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `produits`;
+CREATE TABLE `produits` (
+  `idProduit` int(11) NOT NULL,
   `label` text NOT NULL,
   `categorieProduit` int(11) NOT NULL,
   `description` longtext NOT NULL,
   `prix` int(11) NOT NULL,
-  PRIMARY KEY (`idProduit`),
-  KEY `categorieProduit` (`categorieProduit`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
-
---
--- Contenu de la table `produits`
---
-
-INSERT INTO `produits` (`idProduit`, `label`, `categorieProduit`, `description`, `prix`) VALUES
-(1, 'Test', 1, 'Aucune Description', 10);
+  `favorited` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -125,17 +95,12 @@ INSERT INTO `produits` (`idProduit`, `label`, `categorieProduit`, `description`,
 -- Structure de la table `produitsCommandes`
 --
 
-CREATE TABLE IF NOT EXISTS `produitsCommandes` (
+DROP TABLE IF EXISTS `produitsCommandes`;
+CREATE TABLE `produitsCommandes` (
   `idCommande` int(11) NOT NULL,
   `idProduit` int(11) NOT NULL,
-  PRIMARY KEY (`idCommande`,`idProduit`),
-  KEY `idProduit` (`idProduit`)
+  `quantite` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Contenu de la table `produitsCommandes`
---
-
 
 -- --------------------------------------------------------
 
@@ -143,17 +108,12 @@ CREATE TABLE IF NOT EXISTS `produitsCommandes` (
 -- Structure de la table `rangs`
 --
 
-CREATE TABLE IF NOT EXISTS `rangs` (
-  `idRang` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `rangs`;
+CREATE TABLE `rangs` (
+  `idRang` int(11) NOT NULL,
   `label` text NOT NULL,
-  `power` int(11) NOT NULL,
-  PRIMARY KEY (`idRang`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
---
--- Contenu de la table `rangs`
---
-
+  `power` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -161,16 +121,11 @@ CREATE TABLE IF NOT EXISTS `rangs` (
 -- Structure de la table `stocks`
 --
 
-CREATE TABLE IF NOT EXISTS `stocks` (
+DROP TABLE IF EXISTS `stocks`;
+CREATE TABLE `stocks` (
   `idProduit` int(11) NOT NULL,
-  `stockRestant` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`idProduit`)
+  `stockRestant` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Contenu de la table `stocks`
---
-
 
 -- --------------------------------------------------------
 
@@ -178,21 +133,16 @@ CREATE TABLE IF NOT EXISTS `stocks` (
 -- Structure de la table `utilisateurs`
 --
 
-CREATE TABLE IF NOT EXISTS `utilisateurs` (
-  `idUtilisateur` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `utilisateurs`;
+CREATE TABLE `utilisateurs` (
+  `idUtilisateur` int(11) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `prenom` text NOT NULL,
   `nom` text NOT NULL,
   `rang` int(11) DEFAULT NULL,
-  PRIMARY KEY (`idUtilisateur`),
-  KEY `rang` (`rang`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
---
--- Contenu de la table `utilisateurs`
---
-
+  `nonce` varchar(32) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -200,19 +150,127 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
 -- Structure de la table `visuelsProduits`
 --
 
-CREATE TABLE IF NOT EXISTS `visuelsProduits` (
-  `idVisuel` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `visuelsProduits`;
+CREATE TABLE `visuelsProduits` (
+  `idVisuel` int(11) NOT NULL,
   `idProduit` int(11) NOT NULL,
-  `nomImage` text NOT NULL,
-  PRIMARY KEY (`idVisuel`),
-  KEY `idProduit` (`idProduit`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `nomImage` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Contenu de la table `visuelsProduits`
+-- Index pour les tables exportées
 --
 
+--
+-- Index pour la table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`idCategorie`);
 
+--
+-- Index pour la table `commandes`
+--
+ALTER TABLE `commandes`
+  ADD PRIMARY KEY (`idCommande`),
+  ADD KEY `idUtilisateur` (`idUtilisateur`);
+
+--
+-- Index pour la table `livraisons`
+--
+ALTER TABLE `livraisons`
+  ADD PRIMARY KEY (`idLivraison`),
+  ADD KEY `modeLivraison` (`modeLivraison`);
+
+--
+-- Index pour la table `modesLivraisons`
+--
+ALTER TABLE `modesLivraisons`
+  ADD PRIMARY KEY (`idModeLivraison`);
+
+--
+-- Index pour la table `produits`
+--
+ALTER TABLE `produits`
+  ADD PRIMARY KEY (`idProduit`),
+  ADD KEY `categorieProduit` (`categorieProduit`);
+
+--
+-- Index pour la table `produitsCommandes`
+--
+ALTER TABLE `produitsCommandes`
+  ADD PRIMARY KEY (`idCommande`,`idProduit`),
+  ADD KEY `idProduit` (`idProduit`);
+
+--
+-- Index pour la table `rangs`
+--
+ALTER TABLE `rangs`
+  ADD PRIMARY KEY (`idRang`);
+
+--
+-- Index pour la table `stocks`
+--
+ALTER TABLE `stocks`
+  ADD PRIMARY KEY (`idProduit`);
+
+--
+-- Index pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  ADD PRIMARY KEY (`idUtilisateur`),
+  ADD KEY `rang` (`rang`);
+
+--
+-- Index pour la table `visuelsProduits`
+--
+ALTER TABLE `visuelsProduits`
+  ADD PRIMARY KEY (`idVisuel`),
+  ADD KEY `idProduit` (`idProduit`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `idCategorie` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT pour la table `commandes`
+--
+ALTER TABLE `commandes`
+  MODIFY `idCommande` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT pour la table `livraisons`
+--
+ALTER TABLE `livraisons`
+  MODIFY `idLivraison` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `modesLivraisons`
+--
+ALTER TABLE `modesLivraisons`
+  MODIFY `idModeLivraison` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `produits`
+--
+ALTER TABLE `produits`
+  MODIFY `idProduit` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT pour la table `rangs`
+--
+ALTER TABLE `rangs`
+  MODIFY `idRang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  MODIFY `idUtilisateur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT pour la table `visuelsProduits`
+--
+ALTER TABLE `visuelsProduits`
+  MODIFY `idVisuel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- Contraintes pour les tables exportées
 --
@@ -239,8 +297,8 @@ ALTER TABLE `produits`
 -- Contraintes pour la table `produitsCommandes`
 --
 ALTER TABLE `produitsCommandes`
-  ADD CONSTRAINT `produitsCommandes_ibfk_2` FOREIGN KEY (`idProduit`) REFERENCES `produits` (`idProduit`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `produitsCommandes_ibfk_1` FOREIGN KEY (`idCommande`) REFERENCES `commandes` (`idCommande`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `produitsCommandes_ibfk_1` FOREIGN KEY (`idCommande`) REFERENCES `commandes` (`idCommande`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `produitsCommandes_ibfk_2` FOREIGN KEY (`idProduit`) REFERENCES `produits` (`idProduit`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `stocks`
@@ -259,3 +317,7 @@ ALTER TABLE `utilisateurs`
 --
 ALTER TABLE `visuelsProduits`
   ADD CONSTRAINT `visuelsProduits_ibfk_1` FOREIGN KEY (`idProduit`) REFERENCES `produits` (`idProduit`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
